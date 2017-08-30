d8f49e1776a15361b40fc52cf32ff5c096e00352
Jose Carlos Urra Llanusa <jose.urra86@gmail.com>
2017-08-30 13:53:28 +0200

    Adding Interactivity and Change of styles
    Several changes have taken place, including add of styles. But also
    animations. Postions of the buttons are still presenting issues, as well
    as proper layout size

--- addData.js
+++ addData.js
@@ -16,14 +16,24 @@ function adsliderInit(index) {
 
 			document.getElementById("head1").innerHTML = object.content[index].head1;
 
+			// $("#head1").addClass("animated bounce");
+
+			$("#head1").animate({
+				opacity: 0.25,
+				// left: "+=50",
+				height: "toggle"
+			}, 1000, function() {
+				// Animation complete.
+			});
+
+
 			document.getElementById("para").innerHTML = object.content[index].para;
-
-			document.getElementById("img").innerHTML =
-			'<img class="ui fluid image" src="img/' + object.content[index].img + '">';
-
-
-
+			// document.getElementById("img").innerHTML = '<img class="ui fluid image" src="img/' + object.content[index].img + '">';
 			document.getElementById("column2").innerHTML = object.content[index].body;
+			// Jquery animations
+
+
+			//update i variable to move back and forward
 			return index;
 		});
 		return i=index;
