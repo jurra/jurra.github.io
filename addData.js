557ccfd733d888d12a5c26c6977ff8d30ee5cb95
Jose Carlos Urra Llanusa <jose.urra86@gmail.com>
2017-08-30 16:34:46 +0200

    addData.js Functioning
    Next challenge is to get the object from the get.json file out, instead
    of doing all this functions within get.json

--- addData.js
+++ addData.js
@@ -1,3 +1,7 @@
+$.ajaxSetup({
+   async: false
+ });
+
 
 var i=0;
 
@@ -12,14 +16,17 @@ $("#head1, #para, #img").removeClass("animated fadeIn");
 		index = 3;
 	}
 
+
     $.getJSON("data.json", function(object) {
+
+
 			// Jquery animations, add classes first
 			$("#head1, #para, #img").addClass("animated fadeIn");
 			//Populate with data different areas
 			var storeObj = object.content[index];
 			document.getElementById("head1").innerHTML = storeObj.head1;
 			document.getElementById("para").innerHTML = storeObj.para;
-			document.getElementById("img").innerHTML = '<img class="ui fluid image" src="img/' + storeObj.img + '">';
+			document.getElementById("img").innerHTML = '<img class="ui big fluid bordered image" src="img/' + storeObj.img + '">';
 			document.getElementById("column2").innerHTML = storeObj.body;
 
 
